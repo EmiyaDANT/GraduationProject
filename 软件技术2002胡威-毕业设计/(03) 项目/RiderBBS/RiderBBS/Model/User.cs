64a388fd5f7dcaefc5a06906f98b2a2e1f9efa04
Emiya <emiya1079807593@gmail.com>
2022-03-27 12:52:51 +0800

    v1.0

---
+++ 软件技术2002胡威-毕业设计/(03) 项目/RiderBBS/RiderBBS/Model/User.cs
@@ -0,0 +1,12 @@
+﻿using System.ComponentModel.DataAnnotations;
+namespace RiderBBS.Model
+{
+    public class User
+    {
+        public int Id { get; set; }
+        [Required(ErrorMessage ="用户名不能为空")]
+        public string UserName { get; set; }
+        [Required(ErrorMessage = "密码不能为空")]
+        public  string Password { get; set; }
+    }
+}
---
+++ 软件技术2002胡威-毕业设计/(03) 项目/RiderBBS/RiderBBS/MyPages/Home.razor.data.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace RiderBBS.Component
+{
+    public partial class Home
+    {
+    }
+}
